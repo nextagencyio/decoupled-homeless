01251d0927d2a579851f40aeb6171b80dd5a59a9
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:46 -0500

    fix: mark header-dependent routes as dynamic

--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import ImpactStoryCard from '../components/ImpactStoryCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Impact Stories | Haven House Initiative',
